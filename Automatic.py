e58662441de43a5941f7d719965c40b50ed46a31
Sherry <sdalodia3@gmail.com>
2017-05-15 09:47:42 +0530

    exp1 completed

---
+++ Automatic.py
@@ -0,0 +1,6 @@
+import os
+import time
+
+time.sleep(120)
+os.system('sudo python3 /home/sourabh/Desktop/Sherry/Monitor.py')
+os.system('sudo shutdown -r now')
---
+++ exp1/readme_plot.txt
@@ -0,0 +1,6 @@
+plot_int true
+plot_disk false
+plot_core false
+plot_memory false
+plot_disk_rw false
+int_core 0
