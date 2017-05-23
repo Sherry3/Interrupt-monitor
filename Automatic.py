a08959a38dec0498cfc14c418f698c7642abdec7
Sherry <sdalodia3@gmail.com>
2017-05-23 15:37:53 +0530

    Version7.0

--- Automatic.py
+++ Automatic.py
@@ -2,5 +2,9 @@ import os
 import time
 
 time.sleep(120)
+os.system('sudo cat /sys/devices/system/cpu/cpufreq/policy0/cpuinfo_cur_freq >> log_cpu_freq.txt')
+os.system('sudo cat /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq >> log_cpu_freq.txt')
 os.system('python3 /home/sourabh/Desktop/Sherry/Monitor.py')
+os.system('sudo cat /sys/devices/system/cpu/cpufreq/policy0/cpuinfo_cur_freq >> log_cpu_freq.txt')
+os.system('sudo cat /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq >> log_cpu_freq.txt')
 os.system('sudo reboot')
